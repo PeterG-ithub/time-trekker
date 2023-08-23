d5048d65514bc7a8f669999802d8d016c64e5f57
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-23 10:07:35 -0700

    Add StaticPage controller and route with home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  #root "users#show"
+  # root "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '/home', to: 'static_pages#home'
+  root 'static_pages#home'
 end
