e18802489a31a0807b6415785512907d1b2a47f1
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-23 13:34:48 -0700

    Add #new & #create method with new & _form view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,20 @@
 class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(categories_params)
+    if @category.save
+      redirect_to app_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(:title)
+  end
 end
